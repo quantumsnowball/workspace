9c9c9f17979d8d6b8ca0389c9f19ec3063f85e50
Quantum Snowball <quantum.snowball@gmail.com>
2026-04-12 16:18:51 +0800

    refactor: reorganize into class

--- mouse-wheel-anti-flickering/demo.py
+++ mouse-wheel-anti-flickering/demo.py
@@ -5,45 +5,59 @@
 # ///
 
 import threading
+from collections import deque
+from typing import cast
 
 import evdev
-from evdev import UInput
+from evdev import InputDevice, UInput
 from evdev.ecodes import EV_REL, REL_WHEEL, REL_WHEEL_HI_RES
+from evdev.events import InputEvent
 
 # find you device using 'evtest'
 MOUSE_PATH = "/dev/input/event5"  # gamepad
 # MOUSE_PATH = "/dev/input/event16" # mouse
 
 
-def just_grab():
-    real_mouse = evdev.InputDevice(MOUSE_PATH)
-    real_mouse.grab()
-    fake_mouse = UInput.from_device(
-        real_mouse,
-        name="Smooth Wheel Mouse"
-    )
+class WheelBuffer:
+    def __init__(self, dst_dev: InputDevice, *, delay: float) -> None:
+        self._dst_dev = dst_dev
+        self._delay = delay
+        self._history = deque[InputEvent]()
 
-    def delay_event(e):
-        def worker(e):
-            fake_mouse.write(e.type, e.code, e.value)
-            fake_mouse.syn()
-            print(f"{e.type=}, {e.code=}, {e.value=}")
-        t = threading.Timer(1.0, worker, args=(e, ))
+    def _fire(self) -> None:
+        e = self._history.popleft()
+        self._dst_dev.write(e.type, e.code, e.value)
+        self._dst_dev.syn()
+        print(f"{e.type=}, {e.code=}, {e.value=}")
+
+    def append(self, e: InputEvent) -> None:
+        self._history.append(e)
+        t = threading.Timer(self._delay, self._fire)
         t.start()
 
-    for e in real_mouse.read_loop():
-        # debug print
-        # print(f"{e.type=}, {e.code=}, {e.value=}")
 
-        # delay firing the wheel scrolling event by 1 secs
-        if e.type == EV_REL and (e.code == REL_WHEEL or e.code == REL_WHEEL_HI_RES):
-            delay_event(e)
-            continue
+class SmoothMouse:
+    def __init__(self) -> None:
+        self._src_dev = evdev.InputDevice(MOUSE_PATH)
+        self._dst_dev = UInput.from_device(self._src_dev, name="Smooth Wheel Mouse")
+        self._wheel_buffer = WheelBuffer(self._dst_dev, delay=1.0)
 
-        # passthrough all other irrelevant events
-        fake_mouse.write(e.type, e.code, e.value)
-        fake_mouse.syn()
+    def run(self) -> None:
+        self._src_dev.grab()
+        for e in self._src_dev.read_loop():
+            e = cast(InputEvent, e)
+            # print(f"{e.type=}, {e.code=}, {e.value=}")
+
+            # delay firing the wheel scrolling event by 1 secs
+            if e.type == EV_REL and (e.code == REL_WHEEL or e.code == REL_WHEEL_HI_RES):
+                self._wheel_buffer.append(e)
+                continue
+
+            # passthrough all other irrelevant events
+            self._dst_dev.write(e.type, e.code, e.value)
+            self._dst_dev.syn()
 
 
 if __name__ == "__main__":
-    just_grab()
+    mouse = SmoothMouse()
+    mouse.run()
