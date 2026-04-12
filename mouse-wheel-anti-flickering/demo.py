fbb140f963ae0fb6d8df551af70d14fa34eb68da
Quantum Snowball <quantum.snowball@gmail.com>
2026-04-12 15:42:38 +0800

    demo: apply delayed wheel event and issue later

--- mouse-wheel-anti-flickering/demo.py
+++ mouse-wheel-anti-flickering/demo.py
@@ -4,8 +4,11 @@
 # ]
 # ///
 
+import threading
+
 import evdev
 from evdev import UInput
+from evdev.ecodes import EV_REL, REL_WHEEL, REL_WHEEL_HI_RES
 
 # find you device using 'evtest'
 MOUSE_PATH = "/dev/input/event5"  # gamepad
@@ -20,9 +23,22 @@ def just_grab():
         name="Smooth Wheel Mouse"
     )
 
+    def delay_event(e):
+        def worker(e):
+            fake_mouse.write(e.type, e.code, e.value)
+            fake_mouse.syn()
+            print(f"{e.type=}, {e.code=}, {e.value=}")
+        t = threading.Timer(1.0, worker, args=(e, ))
+        t.start()
+
     for e in real_mouse.read_loop():
         # debug print
-        print(f"{e.type=}, {e.code=}, {e.value=}")
+        # print(f"{e.type=}, {e.code=}, {e.value=}")
+
+        # delay firing the wheel scrolling event by 1 secs
+        if e.type == EV_REL and (e.code == REL_WHEEL or e.code == REL_WHEEL_HI_RES):
+            delay_event(e)
+            continue
 
         # passthrough all other irrelevant events
         fake_mouse.write(e.type, e.code, e.value)
