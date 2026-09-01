4212021e43c3deed807256d309ba135aeb56f93a
Quantum Snowball <quantum.snowball@gmail.com>
2026-09-01 20:14:18 +0800

    mini layout script

--- termux/script/__init__.zsh
+++ termux/script/__init__.zsh
@@ -4,3 +4,9 @@ termux.config.restore-from-repo() {
     termux-reload-settings
 }
 
+termux.keyboard.mini() {
+    termux.config.restore-from-repo 
+    base=$XDG_CONFIG_HOME/workspace/termux/
+    cat $base/script/mini.layout >> $base/_/.termux/termux.properties
+    termux-reload-settings
+}
