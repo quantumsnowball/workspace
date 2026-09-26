5674e58ececab5385c9c7ff8bce8684116dcc028
Quantum Snowball <quantum.snowball@gmail.com>
2026-09-26 22:24:11 +0800

    loader.js

---
+++ google-chrome/userscript/youtube.com/youtube-stats/loader.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         YouTube-Stats
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  load missav-tools
+// @author       You
+// @grant        none
+// @match        https://www.youtube.com/*
+// @match        https://youtube.com/*
+// @require      file:///home/<username>/.config/workspace/google-chrome/userscript/youtube.com/youtube-stats/main.js
+// ==/UserScript==
+
+// README
+//
+// Create a new user script in Tempermonkey. Then copy and paste the content of
+// this file into the editor and save. Replace <username> with the real username.
+// Point the require file path to the correct local file path. Also need to enable
+// "Allow access to file URLs" in extensions settings in Chrome.
